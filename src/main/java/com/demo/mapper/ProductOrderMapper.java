e194f626f011847cb64233534d88539f026a55c4
aliuql <aliuql@qq.com>
2023-10-02 10:44:25 +0800

    初始创建项目

---
+++ src/main/java/com/demo/mapper/ProductOrderMapper.java
@@ -0,0 +1,7 @@
+package com.demo.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.demo.model.ProductOrderDO;
+
+public interface ProductOrderMapper extends BaseMapper<ProductOrderDO> {
+}
